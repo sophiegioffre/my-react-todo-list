0cca6bf02f6bab5b11a0ae80e2f6baa1f3e308c1
sophiegioffre <96739146+sophiegioffre@users.noreply.github.com>
2022-05-15 18:17:50 -0400

    Updates add/create functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 //import logo from './logo.svg';
 import { useState } from 'react';
-import { List } from './components/List';
-import './App.css';
+import { ListItem } from './components/ListItem';
 import { AddForm } from './components/AddForm';
+import './App.css';
+
 
 function App() {
-  const [todos, setTodos] = useState('');
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState([
+    {id: 1, task: "brush teeth", isCompleted: false},
+    {id: 2, task: "wash face", isCompleted: false},
+    {id: 3, task: "drink water", isCompleted: false}
+  ]);
   
+
+  const handleClick = (event) => {
+    const updateTodos = [...todos, {task: value, isCompleted: false}];
+    setTodos(updateTodos);
+    setValue('');
+    console.log(updateTodos);
+    event.preventDefault();
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">ToDo List</h1>
-        <List />
-        <AddForm />
+        {todos.map((element) => (
+            <div>
+                <ListItem todos={element} key={element.id} />
+            </div>
+        ))}
+        <AddForm handleClick={handleClick} value={value} setValue={setValue}/>
       </header>
     </div>
   );
